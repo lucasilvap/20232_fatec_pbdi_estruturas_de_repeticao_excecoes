815258da3c4cfb1378e627c2c5223bd74d7c55d1
Lucas Silva <silva.pereira.92@gmail.com>
2023-09-18 17:54:13 -0300

    ap10(estruutura--repeticao): bloco de codigo 2.3.2

--- script.sql
+++ script.sql
@@ -1,3 +1,17 @@
+CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+INT) RETURNS INT AS
+$$
+BEGIN
+	RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+END;
+$$ LANGUAGE plpgsql;
+SELECT valor_aleatorio_entre (2, 10);
+
+
+
+
+
+
 -- CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
 -- INT) RETURNS INT AS
 -- $$
