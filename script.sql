3b3fde3cfec241cf738f9b65d100f1fdad391265
Lucas Silva <silva.pereira.92@gmail.com>
2023-09-18 18:38:15 -0300

    ap10(estrutura-repeticao): bloco de codigo 2.4.5

--- script.sql
+++ script.sql
@@ -1,23 +1,50 @@
 DO
 $$
 DECLARE
-	contador INT := 0;
+	i INT;
+	j INT;
 BEGIN
+	i := 0;
+	<<externo>>
 	LOOP
-		contador := contador + 1;
-		EXIT WHEN contador > 100;
-		-- ignorando iteração da vez quando contador for múltiplo de 7 com
---IF/CONTINUE
-		IF contador % 7 = 0 THEN
-			CONTINUE;
-		END IF;
-		--ignorando iteração da vez quando contador for múltiplo de 11 com
---CONTINUE WHEN
-		CONTINUE WHEN contador % 11 = 0;
-		RAISE NOTICE '%', contador;
+		i := i + 1;
+		EXIT WHEN i > 10;
+		j := 1;
+		<<interno>>
+		LOOP
+			RAISE NOTICE '% %', i, j;
+			j := j + 1;
+			EXIT WHEN j > 10;
+			-- j vai contar até 5, o loop externo vai ser interrompido e o programa acaba
+			EXIT externo WHEN j > 5;
+		END LOOP;
 	END LOOP;
 END;
 $$
+
+
+
+
+-- DO
+-- $$
+-- DECLARE
+-- 	contador INT := 0;
+-- BEGIN
+-- 	LOOP
+-- 		contador := contador + 1;
+-- 		EXIT WHEN contador > 100;
+-- 		-- ignorando iteração da vez quando contador for múltiplo de 7 com
+-- --IF/CONTINUE
+-- 		IF contador % 7 = 0 THEN
+-- 			CONTINUE;
+-- 		END IF;
+-- 		--ignorando iteração da vez quando contador for múltiplo de 11 com
+-- --CONTINUE WHEN
+-- 		CONTINUE WHEN contador % 11 = 0;
+-- 		RAISE NOTICE '%', contador;
+-- 	END LOOP;
+-- END;
+-- $$
 
 
 
