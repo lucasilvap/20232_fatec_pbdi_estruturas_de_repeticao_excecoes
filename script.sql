5ec1c8f2d9a516ed2f937d736e1b120f67c03ffc
Lucas Silva <silva.pereira.92@gmail.com>
2023-09-18 17:57:38 -0300

    ap10(estrutura-repeticao): bloco de código 2.4.1

--- script.sql
+++ script.sql
@@ -1,11 +1,26 @@
-CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
-INT) RETURNS INT AS
+-- Observe como não é condição de continuidade
+-- Estamos diante de um loop infinito
+DO
 $$
 BEGIN
-	RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+	LOOP
+		RAISE NOTICE 'Teste loop simples...';
+	END LOOP;
 END;
-$$ LANGUAGE plpgsql;
-SELECT valor_aleatorio_entre (2, 10);
+$$
+
+
+
+
+
+-- CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+-- INT) RETURNS INT AS
+-- $$
+-- BEGIN
+-- 	RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+-- END;
+-- $$ LANGUAGE plpgsql;
+-- SELECT valor_aleatorio_entre (2, 10);
 
 
 
