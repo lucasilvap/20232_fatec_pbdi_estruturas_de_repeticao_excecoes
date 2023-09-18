48c500d5148a7d1db290b2d9a4ea95631eb78b18
Lucas Silva <silva.pereira.92@gmail.com>
2023-09-18 18:40:12 -0300

    ap10(estrutura-repeticao): bloco de codigo 2.4.6

--- script.sql
+++ script.sql
@@ -15,12 +15,39 @@ BEGIN
 			RAISE NOTICE '% %', i, j;
 			j := j + 1;
 			EXIT WHEN j > 10;
-			-- j vai contar até 5, o loop externo vai ser interrompido e o programa acaba
-			EXIT externo WHEN j > 5;
+			-- j vai contar até 5, o loop interno vai ser interrompido e prosseguimos para a próxima iteração do loop externo
+			CONTINUE externo WHEN j > 5;
 		END LOOP;
 	END LOOP;
 END;
 $$
+
+
+
+
+-- DO
+-- $$
+-- DECLARE
+-- 	i INT;
+-- 	j INT;
+-- BEGIN
+-- 	i := 0;
+-- 	<<externo>>
+-- 	LOOP
+-- 		i := i + 1;
+-- 		EXIT WHEN i > 10;
+-- 		j := 1;
+-- 		<<interno>>
+-- 		LOOP
+-- 			RAISE NOTICE '% %', i, j;
+-- 			j := j + 1;
+-- 			EXIT WHEN j > 10;
+-- 			-- j vai contar até 5, o loop externo vai ser interrompido e o programa acaba
+-- 			EXIT externo WHEN j > 5;
+-- 		END LOOP;
+-- 	END LOOP;
+-- END;
+-- $$
 
 
 
