4a62d8efb171a3be16b88129cd567dafab01e221
Lucas Silva <silva.pereira.92@gmail.com>
2023-09-18 18:11:19 -0300

    ap10(estrutura-repeticao): bloco de codigo 2.4.3

--- script.sql
+++ script.sql
@@ -1,18 +1,38 @@
--- Contando de 1 a 10
--- Saída com IF/EXIT
+- Contando de 1 a 10
+-- Saída com EXIT/WHEN
 DO
 $$
 DECLARE
 	contador INT := 1;
 BEGIN
 	LOOP
-		RAISE NOTICE '%', contador;
-		contador := contador + 1;
-		IF contador > 10 THEN
-			EXIT;
-		END IF;
+	RAISE NOTICE '%', contador;
+	contador := contador + 1;
+	EXIT WHEN contador > 10;
 	END LOOP;
-END;$$
+END;
+$$
+
+
+
+
+
+
+-- -- Contando de 1 a 10
+-- -- Saída com IF/EXIT
+-- DO
+-- $$
+-- DECLARE
+-- 	contador INT := 1;
+-- BEGIN
+-- 	LOOP
+-- 		RAISE NOTICE '%', contador;
+-- 		contador := contador + 1;
+-- 		IF contador > 10 THEN
+-- 			EXIT;
+-- 		END IF;
+-- 	END LOOP;
+-- END;$$
 
 
 
